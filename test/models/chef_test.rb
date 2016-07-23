beebc140301892b74fc16b1839d9434355a11bf8
CHETAN <s.p.chetan11@gmail.com>
2016-07-24 00:35:00 +0530

    added test cases with TDD

--- test/models/chef_test.rb
+++ test/models/chef_test.rb
@@ -3,43 +3,40 @@ require 'test_helper'
 class ChefTest < ActiveSupport::TestCase
   
 def setup
-@chef= Chef.new(chefname: "john", email: "john@example.com")
+	@chef= Chef.new(chefname: "john", email: "john@example.com")
 end
 
-test "chef should be valid" do 
-	assert @chef.valid?
-	
+test "chef should be valid" do
+assert @chef.valid?
 end
 
-test "chefname should be present" do
-	@chef.chefname = ""
-		assert_not @chef.valid?
-	end
-
-test "chefname should not be too long" do
-	@chef.chefname = "a" * 41
-    assert_not @chef.valid?
+test "chef name should be present" do
+	@chef.chefname = "";
+	assert_not @chef.valid?
 end
 
+test "chef name not to be too long" do
+	@chef.chefname = "a"*41;
+	assert_not @chef.valid?
+end
 
 test "chef name should not be too short" do
-	@chef.chefname = "aa"
+    @chef.chefname = "aa";
     assert_not @chef.valid?
 end
 
 test "email should be present" do
-	@chef.email = ""
+	@chef.email = "";
 	assert_not @chef.valid?
 end
 
 test "email length should be within bounds" do
-@chef.email = "a" *101 + "@example.com"
+	@chef.email = "a"*101 + "@example.com"
 	assert_not @chef.valid?
-
 end
 
 test "email should be unique" do
-dup_chef = @chef.dup_chef 
+dup_chef = @chef.dup
 dup_chef.email = @chef.email.upcase
 @chef.save
 assert_not dup_chef.valid?
@@ -48,13 +45,18 @@ end
 test "email validation should accept valid addresses" do
 	valid_addresses = %w[user@eee.com R_TDD-DS@eee.hello.org user@example.com first.last@eem.au]
 	valid_addresses.each do |va|
-    @chef.each = va
+    @chef.email = va
     assert @chef.valid? , '#{va.inspect} should be valid'
 end
 	end
 
-	test "email validation should reject invalid addresses" do
-	end
+ 	test "email validation should reject invalid addresses" do
+     invalid_addresses = %w[user@example,com user_at_org.org user.name@example. eee@i_am.com foo@ee+arr.com]
+     invalid_addresses.each do |ia|
+ 	 @chef.email = ia
+     assert_not @chef.valid?, '#{ia.inspect} should be valid'
+ 	end
+end
 
 
 
--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -2,65 +2,58 @@ require 'test_helper'
 
 class RecipeTest < ActiveSupport::TestCase
 
-  def setup
-  	@recipe = Recipe.new(:name => "chicken", :summary => "best chicken everaaaaaaaaaa", :description => "heat oil, add chicken, cook for 20 min, and then do something, and then do something, and then do something")
-  end
+ def setup
+  @recipe = Recipe.new(name: "chicken parm", summary: "this is the best chicken parm summary ever", description: "heat oil, add onins, add tomato sauce, add chicken, cook for 20 minutes");
+ end
 
-test "recipe should be valid" do
-    assert @recipe.valid?
-end
-
-
- #assert will expect true whereas assert_not will expect false
+ test "recipe should be valid" do
+   assert @recipe.valid?   #assert expects true
+ end
 
 test "name should be present" do
-  	@recipe.name = ""
-  	assert_not @recipe.valid?
+  @recipe.name = "";
+  assert_not @recipe.valid?  #assert_not expects false
 end
 
-  test "name length should not be too long" do
-    @recipe.name = "a" * 101
-    assert_not @recipe.valid?
-end
-
-test "name length should not be too short" do
-  @recipe.name = "aaaa"
+test "name length should not be too long" do
+  @recipe.name = "a"*101;
   assert_not @recipe.valid?
 end
 
-test "summary must be present" do 
-  @recipe.summary = ""
+test "name length should not be too short" do
+  @recipe.name = "aaaa";
+  assert_not @recipe.valid?
+end
+
+test "summary should be present" do
+  @recipe.summary = "";
   assert_not @recipe.valid?
 end
 
 test "summary length should not be too long" do
-    @recipe.summary = "a"* 151
+  @recipe.summary = "a"*151
   assert_not @recipe.valid?
 end
 
 test "summary length should not be too short" do
-  @recipe.summary = "a"* 9
+  @recipe.summary = "a"*9;
   assert_not @recipe.valid?
 end
 
-  test "description should be present" do
-    @recipe.description = ""
-    assert_not @recipe.valid?
-
-end
-
-  test "description should not be too long" do
-     @recipe.description = "a" * 501
-    assert_not @recipe.valid?
-end
-
-test "description should not be too short" do
-      @recipe.description = "a" * 19
-    assert_not @recipe.valid?
+test "description should be present" do
+  @recipe.description = "";
+  assert_not @recipe.valid?
 end
 
 
+test "description length should not be too long" do
+  @recipe.description = "a"*501;
+  assert_not @recipe.valid?
+end
 
-
+test "description length should not be too short" do
+  @recipe.description = "a"*19;
+  assert_not @recipe.valid?
+end
 
 end
